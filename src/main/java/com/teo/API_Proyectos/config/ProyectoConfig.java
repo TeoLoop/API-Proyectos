9c687c2a1ce3a4dbfc0d1f61ae3a3978823754ed
Mateo Lopez <matelopez2830@gmail.com>
2025-04-12 20:29:00 -0300

    Subiendo proyecto portafolio v1

--- src/main/java/com/teo/API_Proyectos/config/ProyectoConfig.java
+++ src/main/java/com/teo/API_Proyectos/config/ProyectoConfig.java
@@ -88,7 +88,7 @@ public class ProyectoConfig {
                         List.of("/portafolio1.png", "/portafolio2.png")
                 );
 
-                repository.saveAll(List.of(p1, p2, p3, p4, p5, p6, p7));
+                repository.saveAll(List.of(p1, p2, p3, p4, p5, p6, p7, p8));
             }
 
         };
